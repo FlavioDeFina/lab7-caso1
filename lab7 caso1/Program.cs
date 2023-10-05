1ce0598442c887496eda5e9e41099001d2f69792
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-10-04 22:34:11 -0500

    Agregar archivos de proyecto.

---
+++ lab7 caso1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7_caso1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el vamor de n: ");
+            int n = int.Parse(Console.ReadLine());
+            float resultado = 0f;
+            float total = 0f;
+            for (int i = 1; i <= n; i++) 
+            {
+                resultado = (16f - i) / (3f * i);
+                Console.WriteLine("Resultado : " + resultado);
+                total = total + resultado;
+            }
+            Console.WriteLine("El total es : "+total);
+            Console.ReadKey();
+        }
+    }
+}
